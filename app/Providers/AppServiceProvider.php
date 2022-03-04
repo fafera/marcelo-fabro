60b17ec08f1379c282b9cb2bdd4883b86b0bb35f
fafera <fafa.capellari@gmail.com>
2022-03-04 15:14:39 -0300

    Comment observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        Quote::observe(QuoteObserver::class);
+        //Quote::observe(QuoteObserver::class);
     }
 }
