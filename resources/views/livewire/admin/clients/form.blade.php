618e55af618215a58a76b4e460c37a29a4ea60db
fafera <fafa.capellari@gmail.com>
2022-04-01 15:33:42 -0300

    Lazy input

--- resources/views/livewire/admin/clients/form.blade.php
+++ resources/views/livewire/admin/clients/form.blade.php
@@ -9,7 +9,7 @@
                 <div class="col-md-12 pr-1">
                     <div class="form-group">
                         <label>Cliente</label>
-                        <input type="text" wire:model="client.name" class="form-control" placeholder="Cliente">
+                        <input type="text" wire:model.lazy="client.name" class="form-control" placeholder="Cliente">
                     </div>
                 </div>
             </div>
@@ -17,19 +17,19 @@
                 <div class="col-md-4 pr-1">
                     <div class="form-group">
                         <label>Telefone</label>
-                        <input type="text" id="phone" data-mask="(00)00000-0000" wire:model="client.phone" class="form-control" placeholder="Telefone">
+                        <input type="text" id="phone" data-mask="(00)00000-0000" wire:model.lazy="client.phone" class="form-control" placeholder="Telefone">
                     </div>
                 </div> 
                 <div class="col-md-4 pr-1">
                     <div class="form-group">
                         <label>CPF</label>
-                        <input type="text" wire:model="client.cpf" data-mask="000.000.000-00" class="form-control" placeholder="CPF">
+                        <input type="text" wire:model.lazy="client.cpf" data-mask="000.000.000-00" class="form-control" placeholder="CPF">
                     </div>
                 </div>
                 <div class="col-md-4 pr-1">
                     <div class="form-group">
                         <label>E-mail</label>
-                        <input type="email" id="email" wire:model="client.email" class="form-control" placeholder="E-mail">
+                        <input type="email" id="email" wire:model.lazy="client.email" class="form-control" placeholder="E-mail">
                     </div>
                 </div>  
             </div>
