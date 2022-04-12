bc0e5e2b5467424e4b2d5dadd4bfc2631520a877
fafera <fafa.capellari@gmail.com>
2022-04-11 23:12:08 -0300

    fixes

--- resources/views/admin/pages/quotes/index.blade.php
+++ resources/views/admin/pages/quotes/index.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="card mb-4">
-                <div class="table-responsive p-3">
+                <div class="p-3">
                     <livewire:admin.quotes.table/>
                 </div>
             </div>
