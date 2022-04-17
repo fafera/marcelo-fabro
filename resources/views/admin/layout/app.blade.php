91ad03961a806fbbd7a6791e685ed9f2b3df2ca5
fafera <fafa.capellari@gmail.com>
2022-04-17 17:53:22 -0300

    fixes

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -8,7 +8,8 @@
     <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
+    <link rel="shortcut icon" href="{{asset('img/fav-marcelo.ico')}}" type="image/x-icon">
+    <link rel="icon" href="{{asset('img/fav-marcelo.ico')}}" type="image/x-icon">
     @include('admin.layout.css')
     @livewireStyles
 </head>
