e873861fef5a03ef3ed95938eb67c844dec6f090
fafera <fafa.capellari@gmail.com>
2022-04-22 14:12:17 -0300

    updates

--- resources/views/front/components/nav.blade.php
+++ resources/views/front/components/nav.blade.php
@@ -2,10 +2,6 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
         <a id="anchor-page-top" style="display:none;" class="navbar-brand js-scroll-trigger" href="#page-top"><img id="logo" style="height: 80px" src="{{asset('img/logo-preto.png')}}"></a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i class="fas fa-bars"></i>
-        </button>
         <ul class="navbar-nav ml-auto nav-font">
             <li class="nav-item">
             <a id="facebook" href="#" class="nav-link">
--- resources/views/front/layout/app.blade.php
+++ resources/views/front/layout/app.blade.php
@@ -4,10 +4,33 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+  <meta name="description" content="Marcelo Fabro - Produção Musical">
+  <meta name="author" content="Fafa Capellari">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- MS Tile - for Microsoft apps-->
+  <meta name="msapplication-TileImage" content="{{asset('img/site.png')}}">    
 
+  <!-- fb & Whatsapp -->
+
+  <!-- Site Name, Title, and Description to be displayed -->
+  <meta property="og:site_name" content="Marcelo Fabro - Produção Musical">
+  <meta property="og:title" content="Marcelo Fabro - Produção Musical para Eventos Marcantes">
+  <meta property="og:description" content="Marcelo Fabro - Produção Musical é a garantia de um evento marcante com momentos encantadores e inesquecíveis embaladas por um repertório totalmente elaborado de acordo com seu gosto.">
+
+  <!-- Image to display -->
+  <!-- Replace   «example.com/image01.jpg» with your own -->
+  <meta property="og:image" content="{{asset('img/site.png')}}">
+
+  <!-- No need to change anything here -->
+  <meta property="og:type" content="website" />
+  <meta property="og:image:type" content="image/jpeg">
+
+  <!-- Size of image. Any size up to 300. Anything above 300px will not work in WhatsApp -->
+  <meta property="og:image:width" content="300">
+  <meta property="og:image:height" content="300">
+
+  <!-- Website to visit when clicked in fb or WhatsApp-->
+  <meta property="og:url" content="https://marcelofabro.com">
   <title>{{config('app.name')}}</title>
 
   <link rel="shortcut icon" href="{{asset('img/fav-marcelo.ico')}}" type="image/x-icon">
